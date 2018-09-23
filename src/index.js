3513612c6e67b29c4f8719bff769ed3d8fa0ecd1
sergeybandar <sergeybandar@mail.ru>
2018-09-23 23:59:44 +0300

    love-triangle

--- src/index.js
+++ src/index.js
@@ -4,4 +4,22 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var a=0,b=0,c=0;
+  var arr=[];
+  for(var i=0;i<preferences.length;i++){
+  	  
+	  arr[i+1]=preferences[i];
+  }
+  for(var i=0; i<arr.length; ){
+  i++;
+  b=arr[i];
+  c=arr[b];
+  
+  if(arr[c]==i){
+  	  a++;
+  }
+  
+  }
+  
+  return Math.floor(a/3);
 };
